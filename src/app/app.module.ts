d16608cf16ca862e2faa3aadca3c4f51a44c5518
alberto.delcampo-at-676770050448 <luis.albertodelcampo@gmail.com>
2019-07-26 16:10:52 -0500

    Comienza el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { APP_ROUTING } from './app.routes';
+import { BreadcrumbsComponent } from './common/breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './common/header/header.component';
+import { NopagefoundComponent } from './common/nopagefound/nopagefound.component';
+import { SidebarComponent } from './common/sidebar/sidebar.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    PagesComponent,
+    RegisterComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, APP_ROUTING],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
